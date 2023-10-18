6941d2e7316373a45160014d2a8987511bda5573
Mark Tearle <mark.tearle@qcif.edu.au>
2023-10-18 10:01:49 +0800

    Add model and tests for passes

---
+++ ckanext/laissezpasser/laissezpasser.py
@@ -0,0 +1,92 @@
+from typing import Any, Iterable, Optional, OrderedDict
+import datetime
+
+import ckan.plugins.toolkit as tk
+from ckan.common import g
+
+CONFIG_PASS_DURATION = "ckanext.laissezpasser.duration"
+CONFIG_PASS_DURATION_DEFAULT = "1"
+
+class LaissezPasser():
+    def __init__(self):
+        self.key = 'laissezpasser'
+        self.duration = int(tk.config.get(CONFIG_PASS_DURATION, CONFIG_PASS_DURATION_DEFAULT))
+        self.site_user = tk.get_action("get_site_user")({'ignore_auth': True}, {})["name"]
+        self.admin_ctx = {"ignore_auth": True, "user": self.site_user }
+        self.clear()
+
+    def clear(self):
+        self.content = {}
+
+    def add(self, item: str, passdatetime = None):
+        # return True on success of adding pass
+        if passdatetime:
+           try:
+               passvaliduntil = datetime.datetime.strptime(passdatetime, '%Y-%m-%dT%H:%M:%S.%f')
+           except ValueError:
+               return False
+        else:
+           stale_after = datetime.timedelta(days=self.duration)
+           now = datetime.datetime.utcnow()
+           passvaliduntil = now + stale_after
+
+        self.content[item] = passvaliduntil.strftime('%Y-%m-%dT%H:%M:%S.%f')
+        return True
+
+    def check(self, item: str):
+        # Returns date of pass if present otherwise None
+        return self.content.get(item, None)
+
+    def valid(self, item: str):
+        # Returns True if contains a valid pass
+        # Returns False otherwise
+        if not self.check(item): return False
+        try:
+            timeofpass = datetime.datetime.strptime(self.check(item), '%Y-%m-%dT%H:%M:%S.%f')
+        except ValueError:
+            return False
+        
+        assume_stale_after = datetime.timedelta(days=self.duration)
+        passdelta = datetime.datetime.utcnow() - timeofpass
+
+        return passdelta < assume_stale_after
+
+    def restore(self):
+        user = self._get_user()
+        plugin_extras = self._get_plugin_extras(user)
+        self.content = self._get_passes(plugin_extras)
+
+    def save(self):
+        user = self._get_user()
+        plugin_extras = self._get_plugin_extras(user)
+        plugin_extras[self.key] = self.content
+        user['plugin_extras'] = plugin_extras
+        tk.get_action('user_update')(self.admin_ctx, user)
+
+    def _get_user(self):
+        # Default to own user
+        username = g.userobj.name
+        # Only admins can see other users passes
+        if g.userobj.name != self.username:
+            if g.userobj.sysadmin is True:
+                username = self.username
+
+        user_id = { "id": username, "include_plugin_extras": True }
+        user = tk.get_action('user_show')(self.admin_ctx, user_id)
+        return user
+
+    def _get_plugin_extras(self, user):
+        if 'plugin_extras' in user and user.get('plugin_extras') is not None:
+            return user.get('plugin_extras')
+        else:
+            return {}
+    
+    def _get_passes(self, plugin_extras):
+        if plugin_extras is not None and self.key in plugin_extras: 
+            return plugin_extras[key]
+        else:
+            return {}      
+
+    def drop(self):
+        self.clear()
+        self.save(key)
---
+++ ckanext/laissezpasser/tests/test_laissezpasser.py
@@ -0,0 +1,74 @@
+import pytest
+from ckanext.laissezpasser import laissezpasser
+
+
+class TestLaissezPasser:
+    def test_restore(self, laissezpasser):
+        # check no passes 
+        laissezpasser.restore()
+        assert not laissezpasser
+
+        # check no passes after restore from user obj
+        laissezpasser.add("hello")
+        laissezpasser.restore()
+        assert not laissezpasser
+
+        # add add a pass
+        laissezpasser.add("hello")
+        laissezpasser.save()
+
+        laissezpasser.restore()
+        assert laissezpasser
+
+
+    def test_check(self, laissezpasser):
+        # check no passes 
+        laissezpasser.clear()
+        assert not laissezpasser
+
+        # check no pass for hello
+        assert not laissezpasser.check("hello")
+        
+        # add pass
+        laissezpasser.add("hello")
+
+        # check pass for hello
+        assert not laissezpasser.check("fake")
+        assert laissezpasser.check("hello")
+
+        # clear, and then recheck
+        laissezpasser.clear()
+        assert not laissezpasser.check("hello")
+
+
+    def test_valid(self, laissezpasser):
+        # check no passes 
+        laissezpasser.clear()
+        assert not laissezpasser
+
+        # check no pass for hello
+        assert not laissezpasser.check("hello")
+        
+        # check no valid pass for hello
+        assert not laissezpasser.valid("hello")
+
+        # add pass
+        laissezpasser.add("hello")
+
+        # check pass for hello
+        assert not laissezpasser.check("fake")
+        assert laissezpasser.check("hello")
+
+        # check valid pass for hello - defaults
+        assert not laissezpasser.valid("fake")
+        assert laissezpasser.valid("hello")
+
+        # clear, and then recheck
+        laissezpasser.clear()
+        assert not laissezpasser.check("hello")
+        assert not laissezpasser.valid("hello")
+
+        # add pass with expired date
+        laissezpasser.add("hello", passdatetime="2020-07-30T03:17:41.866523")
+        assert laissezpasser.check("hello")
+        assert not laissezpasser.valid("hello")
