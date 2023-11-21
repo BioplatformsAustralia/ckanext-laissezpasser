2a6711e3b7f8dbf7db5c57c9de5bfa7092c99bef
Mark Tearle <mark.tearle@qcif.edu.au>
2023-11-21 09:01:22 +0800

    Implement laissezpasser_check_package, laissezpasser_check_resource and laissezpasser_remove

--- ckanext/laissezpasser/logic/action.py
+++ ckanext/laissezpasser/logic/action.py
@@ -2,6 +2,7 @@ import ckan.plugins.toolkit as tk
 import ckanext.laissezpasser.logic.schema as schema
 import ckan.authz as authz
 
+from ckan.common import g
 from ckanext.laissezpasser import laissezpasser
 
 def _get_plugin_extras(user):
@@ -13,22 +14,81 @@ def _get_plugin_extras(user):
 
 @tk.side_effect_free
 def laissezpasser_check_package(context, data_dict):
+    # Arguments
+    #  package_id (string)
+    #  user_id (string) - (optional)
     tk.check_access(
         "laissezpasser_check_package", context, data_dict)
-    return {}
+
+    data, errors = tk.navl_validate(
+        data_dict, schema.laissezpasser_check_package(), context)
+
+    if errors:
+        raise tk.ValidationError(errors)
+
+    # if user_id is not set, use self
+    effective_id = g.userobj.id
+    user_id = data.get("user_id",None)
+
+    # Raise an error if the user is not a sysadmin but has user_id set
+    if user_id and not authz.is_sysadmin(effective_id):
+        raise tk.ValidationError({
+            'user_id': ['Only sysadmin admins or self can check.']
+        })
+    else:
+        user_id = effective_id
+
+    lp_dict = {
+        "user_id": data.get("user_id"),
+    }
+
+    l = laissezpasser.get_laissezpasser(None,lp_dict)
+    l.restore()
+
+    # get the package and then it's name
+    package_id = data.get("package_id")
+
+    package_dict = tk.get_action("package_show")(
+        dict(context, return_type="dict"), {"id": package_id}
+    )
+
+    package_name = package_dict.get("name")
+
+
+    # return package name and date valid until
+    return {
+        "package_name": package_name,
+        "valid": l.valid(package_name),
+        "valid_until": l.check(package_name),
+    }
 
 
 @tk.side_effect_free
 def laissezpasser_check_resource(context, data_dict):
     tk.check_access(
         "laissezpasser_check_resource", context, data_dict)
+
+    data, errors = tk.navl_validate(
+        data_dict, schema.laissezpasser_check_resource(), context)
+
+    if errors:
+        raise tk.ValidationError(errors)
+
+    # get package from resource_id
+    resource = tk.get_action('resource_show')(context, {'id': data.get("resource_id", "")})
+
+    check_dict = dict(data_dict | { "package_id": resource.get("package_id", None) })
+
+    return laissezpasser_check_package(context, check_dict)
+
+
 def laissezpasser_create(context, data_dict):
-    tk.check_access(
-        "laissezpasser_create", context, data_dict)
     # Arguments
     #  user_id (string)
     #  package_id (string)
     #  valid_until (iso date string) - (optional)
+    tk.check_access(
+        "laissezpasser_create", context, data_dict)
 
     data, errors = tk.navl_validate(
         data_dict, schema.laissezpasser_create(), context)
@@ -42,14 +102,9 @@ def laissezpasser_create(context, data_dict):
                 'user_id': ['User is already a sysadmin.']
             })
 
-    # look at resource_create
-    log.warn(context)
-    log.warn(data_dict)
-
     lp_dict = {
         "user_id": data.get("user_id"),
     }
-    log.warn(lp_dict)
 
     l = laissezpasser.get_laissezpasser(None,lp_dict)
     l.restore()
@@ -79,6 +134,10 @@ def laissezpasser_create(context, data_dict):
 
 
 def laissezpasser_remove(context, data_dict):
+    # Arguments
+    #  user_id (string)
+    #  package_id (string)
+
     tk.check_access(
         "laissezpasser_remove", context, data_dict)
     data, errors = tk.navl_validate(
@@ -86,12 +145,32 @@ def laissezpasser_remove(context, data_dict):
 
     if errors:
         raise tk.ValidationError(errors)
+
+    lp_dict = {
+        "user_id": data.get("user_id"),
+    }
+
+    l = laissezpasser.get_laissezpasser(None,lp_dict)
+    l.restore()
+
+    # get the package and then it's name
+    package_id = data.get("package_id")
+
+    package_dict = tk.get_action("package_show")(
+        dict(context, return_type="dict"), {"id": package_id}
+    )
+
+    package_name = package_dict.get("name")
+
+    l.remove(package_name)
+
+    l.save()
+
     return {}
 
 
 def get_actions():
     return {
-        #'laissezpasser_get_sum': laissezpasser_get_sum,
         'laissezpasser_check_package': laissezpasser_check_package,
         'laissezpasser_check_resource': laissezpasser_check_resource,
         'laissezpasser_create': laissezpasser_create,
--- ckanext/laissezpasser/logic/schema.py
+++ ckanext/laissezpasser/logic/schema.py
@@ -1,6 +1,37 @@
 import ckan.plugins.toolkit as tk
 from six import text_type
 
+def laissezpasser_check_resource():
+    #  user_id (string)
+    #  resource_id (string)
+    not_empty = tk.get_validator("not_empty")
+    ignore_missing = tk.get_validator("ignore_missing")
+    user_id_or_name_exists = tk.get_validator("user_id_or_name_exists")
+    resource_id_exists = tk.get_validator("resource_id_exists")
+    convert_user_name_or_id_to_id = tk.get_validator("convert_user_name_or_id_to_id")
+
+    return {
+        "user_id": [ignore_missing, text_type, user_id_or_name_exists, convert_user_name_or_id_to_id],
+        "resource_id": [not_empty, text_type, resource_id_exists],
+    }
+
+
+def laissezpasser_check_package():
+    #  user_id (string)
+    #  package_id (string)
+    not_empty = tk.get_validator("not_empty")
+    ignore_missing = tk.get_validator("ignore_missing")
+    user_id_or_name_exists = tk.get_validator("user_id_or_name_exists")
+    package_id_or_name_exists = tk.get_validator("package_id_or_name_exists")
+    convert_user_name_or_id_to_id = tk.get_validator("convert_user_name_or_id_to_id")
+    convert_package_name_or_id_to_id = tk.get_validator("convert_package_name_or_id_to_id")
+
+    return {
+        "user_id": [ignore_missing, text_type, user_id_or_name_exists, convert_user_name_or_id_to_id],
+        "package_id": [not_empty, text_type, package_id_or_name_exists, convert_package_name_or_id_to_id],
+    }
+
+
 def laissezpasser_create():
     #  user_id (string)
     #  package_id (string)
